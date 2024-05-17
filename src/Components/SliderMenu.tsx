fb4ee99a491415c51d98af1ba14164c6f8c42b2b
Victor Ciolac <victor.bcfraga@gmail.com>
2024-05-17 04:02:21 -0300

    sliderMenu quase identico

--- src/Components/SliderMenu.tsx
+++ src/Components/SliderMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import rightArrow from '../assets/right-arrow.svg';
 import leftArrow from '../assets/left-arrow.svg';
 import casasNaArvore from '../assets/sliderabnb/casas-na-arvore.jpg';
@@ -20,7 +20,23 @@ import tropical from '../assets/sliderabnb/tropica.jpg';
 import vinhedos from '../assets/sliderabnb/vinhedos.jpg';
 import vistasIncriveis from '../assets/sliderabnb/vistas-incriveis.jpg';
 import iconicos from '../assets/sliderabnb/iconicos.webp';
+import acampamentos from '../assets/sliderabnb/acampamentos.jpg';
+import barcos from '../assets/sliderabnb/barcos.jpg';
+import casasArredondadas from '../assets/sliderabnb/casasArredondadas.jpg';
+import casasBarco from '../assets/sliderabnb/casasBarco.jpg';
+import casasDeTerra from '../assets/sliderabnb/casasDeTerra.jpg';
+import castelos from '../assets/sliderabnb/Castelos.jpg';
+import cidadesFamosas from '../assets/sliderabnb/cidadesFamosas.jpg';
+import containeres from '../assets/sliderabnb/containeres.jpg';
+import diversao from '../assets/sliderabnb/diversao.jpg';
+import espacosCriativos from '../assets/sliderabnb/espacosCriativos.jpg';
+import naBeiraDoLago from '../assets/sliderabnb/naBeiraDoLago.jpg';
+import uau from '../assets/sliderabnb/uau.jpg';
+import artico from '../assets/sliderabnb/artico.jpg';
+import energiaAlternativa from '../assets/sliderabnb/energiaAlternativa.jpg';
+import grutas from '../assets/sliderabnb/grutas.jpg';
 import { Context } from '../Context/Context';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface Option {
   nome: string;
@@ -39,20 +55,74 @@ const options: Option[] = [
   { nome: 'No interior', img: noInterior },
   { nome: 'Vinhedos', img: vinhedos },
   { nome: 'Microcasas', img: microcasas },
-  // { nome: 'Piscinas incríveis', img: piscinasIncriveis },
-  // { nome: 'Pousadas', img: pousadas },
-  // { nome: 'Tropical', img: tropical },
-  // { nome: 'Ilhas', img: ilhas },
-  // { nome: 'Casas na árvore', img: casasNaArvore },
-  // { nome: 'Casas triangulares', img: casasTriangulares },
-  // { nome: 'Lago', img: lago },
-  // { nome: 'Mansões', img: mansoes },
+  { nome: 'Piscinas incríveis', img: piscinasIncriveis },
+  { nome: 'Pousadas', img: pousadas },
+  { nome: 'Tropical', img: tropical },
+  { nome: 'Ilhas', img: ilhas },
+  { nome: 'Casas na árvore', img: casasNaArvore },
+  { nome: 'Casas triangulares', img: casasTriangulares },
+  { nome: 'Lago', img: lago },
+  { nome: 'Mansões', img: mansoes },
+  { nome: 'Acampamentos', img: acampamentos },
+  { nome: 'Barcos', img: barcos },
+  { nome: 'Casas arredondadas', img: casasArredondadas },
+  { nome: 'Casas-Barco', img: casasBarco },
+  { nome: 'Casas de terra', img: casasDeTerra },
+  { nome: 'Castelos', img: castelos },
+  { nome: 'Cidades famosas', img: cidadesFamosas },
+  { nome: 'Contêineres', img: containeres },
+  { nome: 'Diversão', img: diversao },
+  { nome: 'Espaços criativos', img: espacosCriativos },
+  { nome: 'Na beira do lago', img: naBeiraDoLago },
+  { nome: 'Uau!', img: uau },
+  { nome: 'Ártico', img: artico },
+  { nome: 'Energia alternativa', img: energiaAlternativa },
+  { nome: 'Grutas', img: grutas },
 ];
 
 function SliderMenu() {
-  const [arrowClicked, setArrowClicked] = useState(false);
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
   const { headerVisible } = useContext(Context);
+  const [page, setPage] = useState(0);
+  const [sidePage, setSidePage] = useState('');
+  const [visibleOptions, setVisibleOptions] = useState<Option[]>([]);
+  const [key, setKey] = useState<string>('');
+  const [optionsPerPage, setOptionsPerPage] = useState<number>(calculateOptionsPerPage());
+
+  useEffect(() => {
+    function handleResize() {
+      setOptionsPerPage(calculateOptionsPerPage());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    const startIndex = page * optionsPerPage;
+    const endIndex = startIndex + optionsPerPage;
+    const newVisibleOptions = options.slice(startIndex, endIndex);
+    setVisibleOptions(newVisibleOptions);
+    setKey(Date.now().toString());
+  }, [page]);
+
+  function calculateOptionsPerPage() {
+    const screenWidth = window.innerWidth;
+    let optionsPerRow = 4;
+    if (screenWidth >= 640) {
+      optionsPerRow = 6;
+    }
+    if (screenWidth >= 768) {
+      optionsPerRow = 8;
+    }
+    if (screenWidth >= 1024) {
+      optionsPerRow = 11;
+    }
+    if (screenWidth >= 1440) {
+      optionsPerRow = 16;
+    }
+    return optionsPerRow;
+  }
 
   const handleButtonClick = (buttonName: string) => {
     const selected = options.find(option => option.nome === buttonName);
@@ -62,39 +132,89 @@ function SliderMenu() {
   };
 
   const handleRightArrow = () => {
-    setArrowClicked(true);
+    setPage(prevPage => prevPage + 1);
+    setSidePage('right');
   };
 
   const handleLeftArrow = () => {
-    setArrowClicked(false);
+    setPage(prevPage => prevPage - 1);
+    setSidePage('left');
   };
 
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: { type: 'spring', stiffness: 50, delayChildren: 0.5,  },
+    },
+    exit: {
+      opacity: 0,
+      transition: { ease: 'easeOut', duration: 0.5 },
+    },
+  };
+  
+  const itemVariants = {
+    hidden: {
+      opacity: 0,
+      x: sidePage === 'right' ? 100 : (sidePage === 'left' ? -100 : 0),
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: { type: 'spring', stiffness: 50, duration: 1.5 },
+    },
+  };
+
+  const startIndex = page * optionsPerPage;
+  const endIndex = startIndex + optionsPerPage;
+
   return (
-    <div className={`${headerVisible ? 'top-48' : 'top-20 min-h-24'} fixed w-full flex justify-center bg-white items-center`}>
-      {arrowClicked && (
-        <button onClick={handleLeftArrow} className="p-1 rounded-full border border-gray-500 bg-white">
+    <motion.div
+      className={`${headerVisible ? 'top-48' : 'top-20 pt-4'} fixed w-full flex justify-center align-middle bg-white items-center`}
+      variants={containerVariants}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+    >
+      {page > 0 && (
+        <button onClick={handleLeftArrow} className="p-1 rounded-full border border-gray-500 bg-white ml-12 z-10">
           <img src={leftArrow} alt="arrow" className="w-4 h-4" />
         </button>
       )}
-      <div className={`w-full ${arrowClicked ? 'ml-8 mr-14' : 'ml-14 mr-14'} grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-11 gap-20`} >
-        {options.map((option, index) => (
-          <div className={`flex flex-col align-middle text-center justify-center items-center hover:bg-gray-200 hover:shadow-lg hover:rounded-lg hover:text-black`} key={index}>
-            <button className={`${selectedOption === option ? 'underline underline-offset-[13px] decoration-2' : ''}  pb-3 flex flex-col align-middle text-center justify-center items-center`} onClick={() => handleButtonClick(option.nome)}>
-              <img
-                src={option.img}
-                alt={option.nome}
-                key={index}
-                className="w-7 h-7 filter grayscale hover:fill-black "
-              />
-              <span className="text-xs font-medium mt-2 text-gray-600 whitespace-nowrap">{option.nome}</span>
-            </button>
-          </div>
-        ))}
+      <div 
+      className={`w-full ${page > 0 ? 'mr-14 ml-2' : ''}
+      mx-14 grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-11 2xl:grid-cols-16 gap-20`} >
+        <AnimatePresence key={key} mode='wait'>
+          {visibleOptions.map((option, index) => (
+            <motion.div
+              key={index}
+              variants={itemVariants}
+              initial='hidden'
+              animate='show'
+              exit='exit'
+              className={`flex flex-col align-middle text-center justify-center items-center hover:bg-gray-200 hover:shadow-lg hover:rounded-lg hover:text-black`}
+            >
+              <button className={`${selectedOption === option ? 'underline underline-offset-[13px] decoration-2' : ''}  pb-3 flex flex-col align-middle text-center justify-center items-center`} onClick={() => handleButtonClick(option.nome)}>
+                <img
+                  src={option.img}
+                  alt={option.nome}
+                  key={index}
+                  className="w-6 h-6 filter grayscale hover:fill-black "
+                />
+                <span className="text-xs font-medium mt-2 text-gray-600 whitespace-nowrap">{option.nome}</span>
+              </button>
+            </motion.div>
+          ))}
+          </AnimatePresence>
       </div>
-      <button onClick={handleRightArrow} className="p-1 rounded-full border border-gray-500 bg-white mr-12">
-        <img src={rightArrow} alt="arrow" className="w-4 h-4" />
-      </button>
-    </div>
+      {endIndex < options.length && (
+        <button onClick={handleRightArrow} className="p-1 rounded-full border border-gray-500 bg-white mr-12">
+          <img src={rightArrow} alt="arrow" className="w-4 h-4" />
+        </button>
+      )}
+    </motion.div>
   );
 };
 
